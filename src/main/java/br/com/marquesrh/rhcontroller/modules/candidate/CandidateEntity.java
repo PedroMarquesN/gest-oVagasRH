31d888a51f5272be6b8d605e7dadbabea53dbd28
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-04-29 14:39:05 -0300

    Feat: Create table candidate

--- src/main/java/br/com/marquesrh/rhcontroller/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/marquesrh/rhcontroller/modules/candidate/CandidateEntity.java
@@ -1,15 +1,28 @@
 package br.com.marquesrh.rhcontroller.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
     private String name;
     @Pattern(regexp = "^[a-z0-9_-]{3,15}$", message = "Invalid username")
     private String username;
@@ -19,5 +32,8 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
